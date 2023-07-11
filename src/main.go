75ff211a055ce3298be7a3886acca4cf51e9524b
kiwi <akif42@live.nl>
2023-07-11 15:40:33 +0200

    setup

---
+++ src/main.go
@@ -0,0 +1,15 @@
+package src
+
+import (
+	"fmt"
+	_ "html/template"
+	_ "log"
+)
+
+func main() {
+	fmt.Printf("Starting server at port 8080\n")
+
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//
+	//})
+}
